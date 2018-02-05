35dd7d5664f625338967a12b24f96df6f23cb43f
socratim <ldite@student.42.fr>
2018-02-05 19:36:06 +0100

    clean

--- config/server.go
+++
@@ -1,3 +0,0 @@
-package config
-
-const MaxBlockPerMsg = 10
--- util/byte.go
+++
@@ -1,9 +0,0 @@
-package util
-
-func LenDoubleSliceByte(slice [][]byte) int {
-	var size = 0
-	for _, b := range slice {
-		size += len(b)
-	}
-	return size
-}
