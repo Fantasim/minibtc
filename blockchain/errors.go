f175554bf7506c2647f73af3dc329c9ee343c785
fantasim <fantasim.dev@gmail.com>
2018-02-21 23:32:37 +0100

    common block error

---
+++ blockchain/errors.go
@@ -0,0 +1,8 @@
+package blockchain
+
+const (
+	WRONG_POW_ERROR = "wrong block pow."
+	WRONG_BLOCK_TIME_ERROR = "wrong block time."
+	NO_NEXT_TO_TIP_ERROR = "block is not next to tip" 
+	WRONG_BLOCK_PUTS_VALUE = "wrong inputs and outputs repartition" 
+)
